026fb2a1965a473831749a4f63fc2a210b3a25dc
JasonMag <jasonmaglangit@gmail.com>
2023-07-11 22:46:18 +0800

    updated expenses model with accounts and transaction types validations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,10 @@
 class Expense < ApplicationRecord
   belongs_to :user
-  belongs_to :category
 
   validates :amount, presence: true, numericality: { greater_than: 0 }
-  validates :category, presence: true
+  validates :acct_by, presence: true
+  validates :acct_to, presence: true
+  validates :transaction_types_id, presence: true
   validates :date, presence: true
   validates :description, presence: true
 end
