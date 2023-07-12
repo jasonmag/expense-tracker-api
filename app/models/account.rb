444bc624fcdd08ee17b17d5a86eb84ec520e8904
JasonMag <jasonmaglangit@gmail.com>
2023-07-13 00:45:43 +0800

    updated account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,7 @@
 class Account < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :account_types_id, presence: true
+  validates :description, presence: true
 end
