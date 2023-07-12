eb69f268ca7e8856eded049e4467989df3999be5
JasonMag <jasonmaglangit@gmail.com>
2023-07-13 00:45:13 +0800

    updated transaction_types controller

--- app/controllers/transaction_types_controller.rb
+++ app/controllers/transaction_types_controller.rb
@@ -1,4 +1,5 @@
 class TransactionTypesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_transaction_type, only: %i[ show update destroy ]
 
   # GET /transaction_types
