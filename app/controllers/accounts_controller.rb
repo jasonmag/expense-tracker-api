01d65d190881397a1061ce2109974d69dc611547
JasonMag <jasonmaglangit@gmail.com>
2023-07-13 22:57:06 +0800

    updated api acounts responses

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,47 +1,51 @@
 class AccountsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_account, only: %i[ show update destroy ]
 
   # GET /accounts
   def index
-    @accounts = Account.all
+    @accounts = current_user.accounts
 
-    render json: @accounts
+    render json: { status: 'SUCCESS', message: 'Accounts loaded', accounts: @accounts }
   end
 
   # GET /accounts/1
   def show
-    render json: @account
+    render json: { status: 'SUCCESS', message: 'Account loaded', account: @account }
   end
 
   # POST /accounts
   def create
-    @account = Account.new(account_params)
+    @account = current_user.accounts.build(account_params)
 
     if @account.save
-      render json: @account, status: :created, location: @account
+      render json: { status: 'SUCCESS', message: 'Account created', account: @account }
     else
-      render json: @account.errors, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'Account creation failed', errors: @account.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /accounts/1
   def update
     if @account.update(account_params)
-      render json: @account
+      render json: { status: 'SUCCESS', message: 'Account updated', account: @account }
     else
-      render json: @account.errors, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'Account update failed', errors: @account.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   # DELETE /accounts/1
   def destroy
     @account.destroy
+    render json: { status: 'SUCCESS', message: 'Account deleted' }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_account
-      @account = Account.find(params[:id])
+      @account = current_user.accounts.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
