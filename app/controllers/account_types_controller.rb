aae13464ef3d4c2e3b56eb8f5aacd0fe4ac6ec63
JasonMag <jasonmaglangit@gmail.com>
2023-07-13 22:09:32 +0800

    updated api account_type replies

--- app/controllers/account_types_controller.rb
+++ app/controllers/account_types_controller.rb
@@ -1,47 +1,51 @@
 class AccountTypesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_account_type, only: %i[ show update destroy ]
 
   # GET /account_types
   def index
-    @account_types = AccountType.all
+    @account_types = current_user.account_types
 
-    render json: @account_types
+    render json: { status: 'SUCCESS', message: 'Account type loaded', account_type: @account_types }
   end
 
   # GET /account_types/1
   def show
-    render json: @account_type
+    render json: { status: 'SUCCESS', message: 'Account type loaded', account_type: @account_type }
   end
 
   # POST /account_types
   def create
-    @account_type = AccountType.new(account_type_params)
+    @account_type = current_user.account_types.build(account_type_params)
 
     if @account_type.save
-      render json: @account_type, status: :created, location: @account_type
+      render json: { status: 'SUCCESS', message: 'Account type created', account_type: @account_type }
     else
-      render json: @account_type.errors, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'Account type creation failed', errors: @account_type.errors.full_messages },
+             status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /account_types/1
   def update
     if @account_type.update(account_type_params)
-      render json: @account_type
+      render json: { status: 'SUCCESS', message: 'Account type updated', expense: @account_type }
     else
-      render json: @account_type.errors, status: :unprocessable_entity
+      render json: { status: 'ERROR', message: 'Account type update failed', errors: @account_type.errors.full_messages },
+            status: :unprocessable_entity
     end
   end
 
   # DELETE /account_types/1
   def destroy
     @account_type.destroy
+    render json: { status: 'Success', message: 'Account Type Deleted' }
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_account_type
-      @account_type = AccountType.find(params[:id])
+      @account_type = current_user.account_types.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
